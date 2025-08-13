3650c3b2e05e32b09446ff05a0c0390392939a15
Pranit Daphale <pranitworkspace@gmail.com>
2025-08-13 20:34:04 +0530

    fixing bugs for the deployment

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -30,48 +30,53 @@ async function registerController(req, res) {
       expiresIn: "1d",
     });
 
-    res.cookie("token", token, { httpOnly: true, secure: true, sameSite: "none",   // required for cross-site cookies
-    maxAge: 24 * 60 * 60 * 1000 }); // optional: secure flags
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none", // required for cross-site cookies
+      maxAge: 24 * 60 * 60 * 1000,
+    }); // optional: secure flags
     const userToReturn = newUser.toObject();
     delete userToReturn.password;
 
-    res.status(201).json({ message: "User registered successfully", user: userToReturn });
-
+    res
+      .status(201)
+      .json({ message: "User registered successfully", user: userToReturn });
   } catch (err) {
     console.error(err);
     return res.status(500).json({ message: "Server error" });
   }
 }
 
-
 async function loginController(req, res) {
-  const {username,password}= req.body;
-  try{
-    const user = await userModel.findOne({ username:username });
-    if(!user){
+  const { username, password } = req.body;
+  try {
+    const user = await userModel.findOne({ username: username });
+    if (!user) {
       return res.status(400).json({ message: "Invalid username" });
     }
     const isPasswordValid = await bcrypt.compare(password, user.password);
-    if(!isPasswordValid) {
+    if (!isPasswordValid) {
       return res.status(400).json({ message: "Invalid password" });
     }
-    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, { expiresIn: "1d" });
-    res.cookie("token", token,{
-    httpOnly: true,
-    secure: false,        // set to true in production (HTTPS)
-    sameSite: 'none', 
-    maxAge: 24 * 60 * 60 * 1000     
-  });
-    res.status(200).json({ message: "Login successful", user: user.toObject() });
-  }catch(err){
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true, // set to true in production (HTTPS)
+      sameSite: "none",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res
+      .status(200)
+      .json({ message: "Login successful", user: user.toObject() });
+  } catch (err) {
     res.status(500).json({ message: "Server error" });
   }
 }
 
-
-
-
 module.exports = {
   registerController,
-  loginController
+  loginController,
 };
